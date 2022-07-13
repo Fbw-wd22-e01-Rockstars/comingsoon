16c916af1e49a194582ff9c372bbb33aedb1f175
NattyDCI <nbavutti@gmail.com>
2022-07-13 23:17:23 +0200

    exercise finished

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles/main.scss'; // Don't delete :)
 import moment from 'moment';
 // All of your javascript should go here :)
 
-
+/* 
 
 console.log(moment().format());
 
@@ -32,4 +32,35 @@ setInterval(function(){
 
 }, interval);
 
+ 
+ */
 
+
+
+let differenceTime = function(){
+    let birthday = moment("2022-11-19");
+    let currentTime = moment();
+    let diffTime = moment.duration(birthday.diff(currentTime));
+    console.dir(diffTime); // gives you the diff in miliseconds
+    //console.log(diffTime.asDays());
+    let days = Math.floor(diffTime.asDays());
+    let hours = diffTime.hours();
+    let minutes = diffTime.minutes();
+    let seconds = diffTime.seconds();
+    
+    return [`${days} days`, `${hours} hours`, `${minutes} minutes`, `${seconds} seconds`];
+
+} 
+
+
+
+setInterval (() => {
+ let result = differenceTime();
+
+
+let lis = document.querySelectorAll("li");
+    lis[0].innerHTML = result[0]; 
+    lis[2].innerText = result[2];
+    lis[1].innerText = result[1];
+    lis[3].innerText = result[3];
+},1000); 
