cfef9f9f26ddc12be74a2ab9eec67f0e369ab5ea
NattyDCI <nbavutti@gmail.com>
2022-07-13 07:35:07 +0200

    exercise finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import './styles/main.scss'; // Don't delete :)
-import smile from './scripts/smile.js'; // Feel free to delete :)
-
+import moment from 'moment';
 // All of your javascript should go here :)
 
 
-smile() // Feel free to delete :)
+
+console.log(moment().format());
+
+
+
+let eventTime = new Date("2022-12.07");
+let currentTime = new Date().getTime();
+var diffTime = eventTime - currentTime;
+var duration = moment.duration(diffTime*1000, 'milliseconds');
+var interval = 1000;
+
+const seconds = document.querySelector(".seconds");
+const minutes = document.querySelector(".minutes");
+const hours = document.querySelector(".hours");
+const days = document.querySelector(".days");
+
+
+
+setInterval(function(){
+    duration = moment.duration(duration - interval, 'milliseconds');
+    seconds.innerText = duration.seconds() + " seconds";
+    minutes.innerText = duration.minutes() + " minutes";
+    hours.innerText = duration.hours() + " hours";
+    days.innerText = duration.days() + " days";
+
+
+
+}, interval);
+
+
